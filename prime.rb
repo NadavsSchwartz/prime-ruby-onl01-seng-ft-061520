2dcffe1b8a2e57b1c0260ad4db8227468cfe8cdb
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-23 23:51:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,4 @@
 # Add  code here!
-class PrimeNumber
-  def is_prime_kata(number)
-    return false if number == 1    
-    max = Math.sqrt(number)    
-    (2..max).all? { |x| number % x != 0 }
-  end
+def prime?(int)
+Prime.instance.prime?(2)
 end
